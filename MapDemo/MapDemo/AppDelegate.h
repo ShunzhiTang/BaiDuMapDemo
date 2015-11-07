f9887fda692de70ac3c754f63ed418fadc0b8853
tangShunZhi <1046975221@qq.com>
2015-11-06 19:43:44 -0800

    初始化项目

---
+++ MapDemo/MapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by Tsz on 15/11/6.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapDemo/MapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapDemo
+//
+//  Created by Tsz on 15/11/6.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
